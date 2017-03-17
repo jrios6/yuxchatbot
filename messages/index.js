5a04854bd23aec0b5da727b43a34c93cae145db9
Ong Jia Rui <ongjiarui@hotmail.com>
2017-03-17 13:19:48 +0800

    Init

---
+++ messages/index.js
@@ -0,0 +1,55 @@
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+
+const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/5061676d-ffa3-4167-94cf-a8a3cb623dfd?subscription-key=a6b43fbe1d814bc7a807e3db1bd079cd&verbose=true';
+var recogniser = new builder.LuisRecognizer(LuisModelUrl);
+
+var intents = new builder.IntentDialog({recognizers:[recogniser]});
+intents.matches('greeting','/sayHi');
+
+bot.dialog('/sayHi', [
+    function (session, args, next) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
