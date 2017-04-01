2226ad8000a0e385a82107c3b1773a1f764fa551
Ong Jia Rui <ongjiarui@hotmail.com>
2017-04-01 12:57:13 +0800

    Removed irrelevant dialogs

--- messages/dialogs/help.js
+++ messages/dialogs/help.js
@@ -3,8 +3,6 @@
 let yuxfeatures = [
   function (session) {
       session.send("If you give me a picture, I can find clothes that look similar to it or combinations of clothes from Zalora that looks good with it.");
-      //session.send("I can also tell you if a piece of clothing or pair of shoes is a good buy! :)");
-      //session.send("I can also give you basic fashion advice or tell you more about recent trends in fashion.");
       session.endDialog();
   },
 ]
